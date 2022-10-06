f6629c9d1bbd0b7a7869c8d4449ce52474a1b048
jacky32 <patrki@atlas.cz>
2022-10-06 20:51:10 +0200

    Add general info form

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -1,23 +1,66 @@
 import React, { Component } from "react";
 import "./Section.css";
+import InputField from "./InputField";
 
 export default class GeneralInformation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      email: "",
-      phoneNumber: "",
+      name: "Name name",
+      email: "email@mail.com",
+      phoneNumber: "+123 456 789 987",
+      isToggledOn: false,
     };
   }
+
+  setUser = (e) => {
+    e.preventDefault();
+    const name = document.getElementById("userName").value;
+    const email = document.getElementById("userEmail").value;
+    const phone = document.getElementById("userPhone").value;
+    this.setState(
+      {
+        name: name,
+        email: email,
+        phone: phone,
+      },
+      () => {
+        this.toggleForm();
+      }
+    );
+  };
+
+  toggleForm = () => {
+    const form = document.getElementById("GeneralInformationForm");
+    const user = document.getElementById("UserInformation");
+    form.classList.toggle("Hidden");
+    user.classList.toggle("Hidden");
+  };
 
   render() {
     return (
       <div className="Section">
-        <div className="SectionName">General Information</div>
-        <div className="SectionItem">Name:</div>
-        <div className="SectionItem">Email:</div>
-        <div className="SectionItem">Phone Number:</div>
+        <div className="SectionName">
+          <div></div>
+          <button onClick={this.toggleForm}>Edit</button>
+        </div>
+        <form
+          id="GeneralInformationForm"
+          className="SectionForm"
+          onSubmit={this.setUser}
+        >
+          <InputField inputId="userName" tag="Name:" type="text" />
+          <InputField inputId="userEmail" tag="Email:" type="email" />
+          <InputField inputId="userPhone" tag="Phone number:" type="tel" />
+          <button type="submit">Submit</button>
+        </form>
+        <div id="UserInformation" className="Hidden">
+          <div>
+            <strong>{this.state.name}</strong>
+          </div>
+          <div>{this.state.email}</div>
+          <div>{this.state.phoneNumber}</div>
+        </div>
       </div>
     );
   }
