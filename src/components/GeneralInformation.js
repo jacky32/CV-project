52c23b4f43b16da169a827a8cd5c03b936d5c2e7
jacky32 <patrki@atlas.cz>
2022-10-06 16:46:26 +0200

    Add GeneralInformation section

---
+++ src/components/GeneralInformation.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Section.css";
+
+export default class GeneralInformation extends Component {
+  render() {
+    return (
+      <div className="GeneralInformation">
+        GeneralInformation
+        <div></div>
+      </div>
+    );
+  }
+}
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,10 +1,12 @@
 import React from "react";
+import GeneralInformation from "./GeneralInformation";
 import "./MainSection.css";
 
 export default function MainSection() {
   return (
     <div className="MainSection">
       <div>Main Section</div>
+      <GeneralInformation />
       <div>Section 2</div>
     </div>
   );
