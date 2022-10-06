a6c026d772b2fdc7e77763ebc4d2ba041564d67c
jacky32 <patrki@atlas.cz>
2022-10-06 16:29:14 +0200

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="Header">
+        <div>Div1</div>
+        <div>CV Project App</div>
+        <div>Div3</div>
+      </header>
+    );
+  }
+}
