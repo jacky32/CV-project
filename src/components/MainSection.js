c3a494385c9736267df9e3666910f4033ded9956
jacky32 <patrki@atlas.cz>
2022-10-06 16:42:27 +0200

    Add main section

---
+++ src/components/MainSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./MainSection.css";
+
+export default function MainSection() {
+  return (
+    <div className="MainSection">
+      <div>Main Section</div>
+      <div>Section 2</div>
+    </div>
+  );
+}
